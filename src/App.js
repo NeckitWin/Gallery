6aadd7d8333cb7fa1493c6098bcb91c090532856
NeckitWin <neckit.v@gmail.com>
2024-03-13 09:30:04 +0100

    fix for preview gh pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Main from "./components/Pages/Main";
-import News from "./components/Pages/News";
+import Buy from "./components/Pages/Buy";
 
 
 const App = () => {
@@ -11,7 +11,8 @@ const App = () => {
             <main>
                 <Routes>
                     <Route path='/' element={<Main />} />
-                    <Route path='/News' element={<News />} />
+                    <Route path='/Gallery' element={<Main />} />
+                    <Route path='/buy-art' element={<Buy />} />
                 </Routes>
 
             </main>
---
+++ src/components/Pages/Buy/index.jsx
@@ -0,0 +1,11 @@
+import s from "./Buy.module.css";
+
+const Buy = () => {
+    return (
+        <div className={s.div}>
+            <h2>Buy art</h2>
+        </div>
+    )
+}
+
+export default Buy;
